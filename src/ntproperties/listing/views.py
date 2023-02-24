c734dc1e1e15246a49d62cd5e582284f5c39400e
Severe Tsunamist <severetsunamist@gmail.com>
2023-02-24 21:25:49 +0300

    db input output v1.0 works

--- src/ntproperties/listing/views.py
+++ src/ntproperties/listing/views.py
@@ -4,12 +4,18 @@ from .models import Property
 from .forms import PropertyForm
 
 def home_view(request):
-    return render(request, "home_navbar.html", {})
+    prop = Property.objects.all().values()
+    context = {
+        'prop': prop
+    }
+
+    return render(request, "home_navbar.html", context)
 
 def property_create_view(request):
     form = PropertyForm(request.POST or None)
     if form.is_valid():
         form.save()
+        form = PropertyForm()
 
     context = {
         'form': form
